80e7bffb95fc82fc03d383c1a1e039154b2b8f5d
Ricky Thoudam <rickythoudam123@gmail.com>
2026-04-01 19:56:12 +0530

    Initial commit

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: [true, "Username already exists"],
+        required: [true, "Username is required"],
+    },
+    email: {
+        type: String,
+        unique: [true, "Email already exists"],
+        required: [true, "Email is required"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    bio: String,
+    followers: {
+        type: Number
+    },
+    profile_img: {
+        type: String,
+        
+    }
+})
