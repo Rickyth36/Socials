b8ecb5de3c283f46f1fd866dc00fd90c5f2b1948
Ricky Thoudam <rickythoudam123@gmail.com>
2026-04-15 21:16:18 +0530

    post apis updated

--- Backend/src/routes/posts.routes.js
+++ Backend/src/routes/posts.routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { createPostController } = require('../controllers/post.controller');
+const postController = require('../controllers/post.controller');
+// const { createPostController } = require('../controllers/post.controller');
 const postRouter = express.Router();
 const multer = require('multer');
 const upload = multer({storage: multer.memoryStorage()});
 
 
-postRouter.post('/',upload.single('file'), createPostController); 
+postRouter.post('/',upload.single('file'), postController.createPostController); 
+
+postRouter.get('/',postController.getPostController);
+
+postRouter.get('/details/:postId', postController.getPostDetails)
 
 module.exports = postRouter;
